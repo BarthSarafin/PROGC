ff419039a8bc206a29284e18ca0d10282157e83c
bluntmosphere <nico.greutmann@gmail.com>
2016-03-07 10:31:04 +0100

    initial update

--- PROGC_Praktikum/PROGC_C_P1/SelbstStudium/03_WoerterZaehlen.c
+++ PROGC_Praktikum/PROGC_C_P1/SelbstStudium/03_WoerterZaehlen.c
@@ -1,21 +1,42 @@
 #include <stdio.h>
+#include <ctype.h>
+int c;
+int previousChar;
+int anzahlZeichen = 0;
+int anzahlWörter = 1;
+
+int isSpaceOrTab(int s){
+    if(s == ' ' || s == '\t') {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+};
+
+void countSpace () {
+    while ((c = getchar()) !='\n') {
+        if (isSpaceOrTab(c) && !isSpaceOrTab(previousChar) ) {
+
+            printf("loop");
+            anzahlWörter++;
+        }
+        if(isSpaceOrTab(c)){
+            anzahlZeichen--;
+        }
+        anzahlZeichen++;
+        previousChar = c;
+    }
+}
+
+
 
 int main (void) {
 
-    int c;
-    int anzahlZeichen = 0;
-    int wortZwischenRaeume = 0;
-    int anzahlWörter = 0;
-
 //for every char read anzhalZeichen is incremented until return is pressed
     (void)printf("Type your text and after pressing return it will be counted.\n");
-        while( (c = getchar()) !='\n') {
-        if ((c == ' ') || (c == '\t')) {
-            wortZwischenRaeume++;
-        }
-        anzahlZeichen++;
-    }
+    countSpace();
 //prints out the charcount
-    (void)printf(" -->Anzahl Zeichen im Satz: %d \n",anzahlZeichen-wortZwischenRaeume);
+    (void)printf(" -->Anzahl Zeichen im Satz ohne Leerzeichen: %d \n",anzahlZeichen);
     (void)printf(" -->Anzahl Wörter im Satz: %d \n",anzahlWörter);
 }
